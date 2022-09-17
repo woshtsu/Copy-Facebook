df3f3e02a371e53b157936ab07da6a0ce7c7e107
woshtsu <113321163+woshtsu@users.noreply.github.com>
2022-09-16 23:15:39 -0500

    agregado javas

---
+++ palosdatosxd.js
@@ -0,0 +1,8 @@
+function Tania(){
+	var nombre, contraseña;
+	nombre = document.getElementById("name").value;
+	contraseña = document.getElementById("pass").value;
+	alert(nombre);
+	alert(contraseña);
+
+}
